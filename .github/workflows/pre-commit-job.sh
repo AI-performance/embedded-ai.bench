b5595e6f3e829c2de45b6d3a07bbb2628306a2dd
Yuan Shuai <ysh329@sina.com>
2020-06-24 03:14:19 +0000

    fix pre-commit

--- .github/workflows/pre-commit-job.sh
+++ .github/workflows/pre-commit-job.sh
@@ -1,27 +1,64 @@
 #!/usr/bin/env bash
+set -ex
+
 function abort() {
     echo "Your change doesn't follow embedded-ai.bench's code style" 1>&2
     echo "Please use pre-commit to auto-format your code." 1>&2
     exit 1
 }
 
-trap 'abort' 0
-set -e
-cd `dirname $0`
-cd ..
-export PATH=/usr/bin:$PATH
-pip install wheel
-pip install pre-commit
-pre-commit install
+function install_miniconda() {
+    #hash conda 2>/dev/null || { echo >&2 "I require foo but it's not installed.  Aborting."; return; }
+
+    apt update
+    apt install -y wget git
+    wget -c https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    chmod +x Miniconda3-latest-Linux-x86_64.sh
+    ./Miniconda3-latest-Linux-x86_64.sh -b
+
+    echo """
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/root/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/root/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/root/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/root/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+""" >> ~/.bashrc
+    source ~/.bashrc
+
+    source /root/miniconda3/bin/activate
+    conda create --yes --quiet --name dev-env-py python
+    conda activate dev-env-py
+
+    python3 -m pip install pre-commit
+    #alias pre-commit=/root/miniconda3/envs/dev-env-py/bin/pre-commit
+    pre-commit install
+    conda init bash
 
 # note(ysh329): enable below for C/C++
 # which clang-format
 # clang-format --version
 
-if ! pre-commit run -a ; then
-  ls -lh
-  git diff  --exit-code
-  exit 1
-fi
+    if ! pre-commit run -a ; then
+        ls -lh
+        git diff --exit-code
+        exit 1
+    fi
 
-trap : 0
+    trap : 0
+}
+
+trap 'abort' 0
+cd `dirname $0`
+cd ..
+export PATH=/usr/bin:$PATH
+
+install_miniconda

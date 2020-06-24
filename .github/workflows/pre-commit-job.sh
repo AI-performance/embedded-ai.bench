f677b4e62de85965ca93df3ab003c468c89fb2c0
Yuan Shuai <ysh329@sina.com>
2020-06-24 03:21:39 +0000

    fix pre-commit

--- .github/workflows/pre-commit-job.sh
+++ .github/workflows/pre-commit-job.sh
@@ -19,14 +19,14 @@ function install_miniconda() {
     echo """
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/root/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('${PREFIX}/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/root/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/root/miniconda3/etc/profile.d/conda.sh"
+    if [ -f "${PREFIX}/etc/profile.d/conda.sh" ]; then
+        . "${PREFIX}/etc/profile.d/conda.sh"
     else
-        export PATH="/root/miniconda3/bin:$PATH"
+        export PATH="${PREFIX}/bin:$PATH"
     fi
 fi
 unset __conda_setup
@@ -34,7 +34,7 @@ unset __conda_setup
 """ >> ~/.bashrc
     source ~/.bashrc
 
-    source /root/miniconda3/bin/activate
+    source ${PREFIX}/bin/activate
     conda create --yes --quiet --name dev-env-py python
     conda activate dev-env-py
 

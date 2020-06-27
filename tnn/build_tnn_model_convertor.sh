966cfdd95ff68268eee75b959b59377c592aafcc
NotBad <ysh329@sina.com>
2020-06-27 21:53:41 +0800

    add tnn model convertor build to ci

---
+++ tnn/build_tnn_model_convertor.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -ex
+
+function prepare_env() {
+    # default
+    pip3 install onnx==1.6.0 onnxruntime numpy onnx-simplifier
+
+    # optional: tensorflow
+    pip3 install tensorflow==1.15.0
+    pip3 install tf2onnx
+    pip3 install onnxruntime
+
+    # optional: caffe
+    sudo apt-get install libprotobuf-dev protobuf-compiler
+}
+
+function main() {
+    prepare_env
+
+    # build
+    cd tnn/tools/convert2tnn
+    ./build.sh
+}
+
+main

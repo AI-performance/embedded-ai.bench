ba058639855a381a1ea95bf1206948901d70332f
NotBad <ysh329@sina.com>
2020-09-06 22:09:03 +0800

    add tflite for clean bench result.sh

--- clean_bench_result.sh
+++ clean_bench_result.sh
@@ -3,7 +3,9 @@ set -x
 mkdir -p bench_backup/ncnn
 mkdir -p bench_backup/mnn
 mkdir -p bench_backup/tnn
+mkdir -p bench_backup/tflite
 
 mv ./ncnn/*.csv bench_backup/ncnn
 mv ./tnn/*.csv bench_backup/tnn
 mv ./mnn/*.csv bench_backup/mnn
+mv ./tflite/*.csv bench_backup/tflite
